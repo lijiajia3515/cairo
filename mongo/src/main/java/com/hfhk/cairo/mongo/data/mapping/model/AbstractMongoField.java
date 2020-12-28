680ecf3845f3bea44858e5d8b4fd6fb940477514
lijiajia <lijiajia3515@163.com>
2020-12-28 16:54:02 +0800

    这个故事还没有完结。——亚索

---
+++ mongo/src/main/java/com/hfhk/cairo/mongo/data/mapping/model/AbstractMongoField.java
@@ -0,0 +1,23 @@
+package com.hfhk.cairo.mongo.data.mapping.model;
+
+import java.util.Optional;
+
+public abstract class AbstractMongoField {
+	private final AbstractMongoField PARENT;
+	private final String SELF;
+
+	public AbstractMongoField() {
+		PARENT = null;
+		SELF = "";
+	}
+
+	public AbstractMongoField(AbstractMongoField PARENT, String prefix) {
+		this.PARENT = PARENT;
+		this.SELF = Optional.ofNullable(PARENT.SELF).map(x -> x.concat(".")).orElse("").concat(prefix);
+	}
+
+	public String field(String fieldName) {
+		return SELF.isBlank() ? fieldName : Optional.ofNullable(fieldName).map(x -> SELF.concat(".").concat(x)).orElse(SELF);
+	}
+
+}
