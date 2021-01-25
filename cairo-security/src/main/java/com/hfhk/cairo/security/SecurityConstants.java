4b39454e29d248100d37e31eeeea5258a023e0c0
lijiajia <lijiajia3515@163.com>
2021-01-25 17:23:52 +0800

    这个故事还没有完结。——亚索

---
+++ cairo-security/src/main/java/com/hfhk/cairo/security/SecurityConstants.java
@@ -0,0 +1,14 @@
+package com.hfhk.cairo.security;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class SecurityConstants {
+	public static final class Role {
+		public static final String ADMIN = "admin";
+
+		public boolean isAdmin(Collection<String> codes) {
+			return Optional.ofNullable(codes).filter(x -> x.contains(ADMIN)).isPresent();
+		}
+	}
+}
