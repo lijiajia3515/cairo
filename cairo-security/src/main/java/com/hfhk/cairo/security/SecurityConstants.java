4ca39c93fd8533d17f02f1e38207b59e76f06061
lijiajia <lijiajia3515@163.com>
2021-01-26 08:48:02 +0800

    这个故事还没有完结。——亚索

--- cairo-security/src/main/java/com/hfhk/cairo/security/SecurityConstants.java
+++ cairo-security/src/main/java/com/hfhk/cairo/security/SecurityConstants.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 public class SecurityConstants {
 	public static final class Role {
-		public static final String ADMIN = "admin";
+		public static final String ADMIN = "ADMIN";
 
 		public boolean isAdmin(Collection<String> codes) {
 			return Optional.ofNullable(codes).filter(x -> x.contains(ADMIN)).isPresent();
